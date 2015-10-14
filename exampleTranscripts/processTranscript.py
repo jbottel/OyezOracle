b533d5afecb4d9d622743e3a3d23714743c44bfc
Jacob Bottelberghe <j.bottelberghe@gmail.com>
2015-10-13 23:11:24 -0600

    Add methods for plotting important numbers
    These include:
    
    - get_plot_names_and_numbers, which is a generalized simple plot to plot
      a bar chart of names and numbers to go along with it.
    
    - get_plot_number_of_statements_per_speaker, which is a function that
      uses the get_plot_names_and_numbers function to plot the number of
      statements per speaker
    
    - get_plot_number_of_words_per_speaker, likewise but for words
    
    A set of helper functions also are added which are fairly self-explanatory.

--- exampleTranscripts/processTranscript.py
+++ exampleTranscripts/processTranscript.py
@@ -5,6 +5,11 @@ import string
 import logging
 import nameparser
 import itertools
+import matplotlib.pyplot as plt
+import numpy
+from mpltools import style
+from mpltools import layout
+
 PDFTOTEXT_COMMAND = "pdftotext"
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -241,6 +246,7 @@ def get_statements_in_argument(argument_text, arguer_name):
 
 
 def get_statistics_from_statements(grouping_of_statements):
+    num_of_words_per_speaker = []
     for speaker, statements in grouping_of_statements.iteritems():
         print "\n" + "-"*60
         print "Statistics for speaker %s " %speaker
@@ -248,21 +254,88 @@ def get_statistics_from_statements(grouping_of_statements):
         print "Number of statements: %d " % len(statements)
         all_words =[]
         for statement in statements:
-            # Remove punctuation in string
-            statement = statement.translate(string.maketrans("",""), string.punctuation)
-            # Split into list of words 
-            words = statement.split()
-            from nltk.corpus import stopwords
-            stopwords = stopwords.words('english')
-            # Remove words from list if they are stopwords
-            words = [word.lower() for word in words if word.lower() not in stopwords]
-            # Add words to the all_words list
+            words = get_words_from_string(statement) 
             all_words.extend(words)
+
         print "Number of words: %d" % len(all_words)
         import nltk
         fdist = nltk.FreqDist(all_words)
         print "Most commonly used words: %s" % fdist.most_common(3)
+        num_of_words_per_speaker.append((speaker,len(all_words)))
+    return num_of_words_per_speaker
+
+def get_words_from_string(statement, exclude_stopwords=True):
+    # Remove punctuation in string
+    statement = statement.translate(string.maketrans("",""), string.punctuation)
+    # Split into list of words 
+    words = statement.split()
+    if exclude_stopwords:
+        from nltk.corpus import stopwords
+        stopwords = stopwords.words('english')
+        # Remove words from list if they are stopwords
+        words = [word.lower() for word in words if word.lower() not in stopwords]
+    return words
+
+
+def get_number_of_words_per_speaker(grouping_of_statements):
+    num_of_words_per_speaker = []
+    for speaker, statements in grouping_of_statements.iteritems():
+        all_words = []
+        for statement in statements:
+            words = get_words_from_string(statement)
+            all_words.extend(words)
+        # Append a tuple of (speaker, number_of_words) to list
+        num_of_words_per_speaker.append((speaker, len(all_words)))
+
+    return num_of_words_per_speaker
     
+
+def get_plot_number_of_words_per_speaker(num_words, ignore_petitioner=False, petitioner=None):
+    """Plot a number of words per speaker. 
+    num_words is a list of tuples of (speaker, num_words_for_speaker)
+    
+    One can pass in a petitioner to ignore for plotting convenience.
+    """
+
+    if ignore_petitioner:
+        num_words = [s for s in num_words if s[0] != petitioner]
+
+    # A list comprehension where we compile the list of tuples into two lists (converting the HumanName into a last name string)
+    speakers, num_words = [str(e[0].last) for e in num_words], [e[1] for e in num_words]
+
+    # Call the plot_names_and_numbers method and plot the speakers and their num_words
+    plot = get_plot_names_and_numbers(speakers, num_words, "Number of Words", "Number of Words Spoken in Argument")
+
+    return plot
+
+def get_plot_number_of_statements_per_speaker(grouping_of_statements, ignore_petitioner=False, petitioner=None):
+    names = []
+    numbers = []
+    for speaker, statements in grouping_of_statements.iteritems():
+        if speaker is petitioner:
+            continue
+        names.append(speaker.last)
+        numbers.append(len(statements))
+
+    plot = get_plot_names_and_numbers(names, numbers, "Number of Interruptions","Number of Individual Interruptions in Argument")
+
+    return plot
+
+
+    
+
+def get_plot_names_and_numbers(names, numbers, x_label=None, title=None):
+    style.use('ggplot')
+    y_positions = numpy.arange(len(names))
+    plt.barh(y_positions,numbers, align='center')
+    plt.yticks(y_positions, names)
+    plt.xlabel(x_label)
+    plt.title(title)
+    return plt
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -271,11 +344,21 @@ if __name__ == "__main__":
             transcript = get_transcript_from_PDF(arg)
             petitioners, respondents = get_petitioners_and_respondents(transcript)
             argument = get_argument(transcript)
+
             arguments_by_advocate = get_arguments_by_advocate(petitioners,respondents,argument)
             for petitioner,argument in arguments_by_advocate["petitioner"].iteritems():
                 statements = get_statements_in_argument(argument, petitioner)
-                print get_statistics_from_statements(statements)
+                get_statistics_from_statements(statements)
+                number_of_words_per_speaker = get_number_of_words_per_speaker(statements)
+                nwsplot = get_plot_number_of_words_per_speaker(number_of_words_per_speaker, ignore_petitioner=True, petitioner=petitioner)
+                nwsplot.show()
+                niaplot = get_plot_number_of_statements_per_speaker(statements,ignore_petitioner=True, petitioner=petitioner)
+                niaplot.show()
+
+                
             for respondent,argument in arguments_by_advocate["respondent"].iteritems():
                 statements = get_statements_in_argument(argument, respondent)
                 #print statements
+
+
 
